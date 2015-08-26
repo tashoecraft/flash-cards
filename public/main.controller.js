14fff3b254f12e43466fca862d97455020d57945
Austin <tashoecraft@gmail.com>
2015-08-26 17:37:20 -0400

    up to organizing

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,31 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function($scope, FlashCardsFactory,
+	ScoreFactory) {
 
-	$scope.answerQuestion = function (answer, flashCard) {
+	$scope.categories = [
+		'MongoDB',
+		'Express',
+		'Angular',
+		'Node'
+	];
+
+	$scope.getCategoryCards = function(category) {
+		FlashCardsFactory.getFlashCards(category).then(function(data) {
+			$scope.flashCards = data;
+		});
+	};
+
+	$scope.getCategoryCards();
+
+
+	$scope.answerQuestion = function(answer, flashCard) {
 		if (!flashCard.answered) {
 			flashCard.answered = true;
 			flashCard.answeredCorrectly = answer.correct;
+			if (answer.correct) {
+				ScoreFactory.correct++;
+			} else {
+				ScoreFactory.incorrect++;
+			}
 		}
-	}
+	};
 });
